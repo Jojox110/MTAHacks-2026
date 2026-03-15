21e61e2b722454948c51ff42fa139f19fe718719
Alexandre Beaulieu <abeaulieu815@gmail.com>
2026-03-14 21:25:29 -0300

    fixed

--- Backend/database.py
+++ Backend/database.py
@@ -2,7 +2,7 @@ import sqlite3
 from config import DB_PATH
 
 def get_db():
-    conn = sqlite3.connect(DB_PATH)
+    conn = sqlite3.connect(DB_PATH, check_same_thread=False)
     conn.row_factory = sqlite3.Row
     conn.execute("PRAGMA foreign_keys = ON")
     try:
